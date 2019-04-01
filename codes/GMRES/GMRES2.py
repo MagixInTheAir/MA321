97a43978fda564134b176083be663e6ec0bf1cf5
Unknown <fulminata27@gmail.com>
2019-04-01 09:00:38 +0200

    GMRES + krylov

--- codes/GMRES/GMRES2.py
+++ codes/GMRES/GMRES2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse.linalg as sc
+import matplotlib.pyplot as plt
 
 
 def GMRES(A, b, espilon):
@@ -24,24 +25,28 @@ def GMRES(A, b, espilon):
     return mat_q[:max_iter, :max_iter] @ y
 
 
-n = 2
+n = 300
 """
 A = np.array([[1., 5, 4, 8, 9, 12], [2, 3, 5, 8, 4, 3], [0, 5, 3, 8, 9, 1], [2, 4, 6, 8, 1, 3], [5, 8, 6, 4, 7, 10], [0, 0, 5, 6, 8, 6]])
 b = np.array([[172], [93], [102], [83], [190], [115]])
 x0 = np.zeros((6, 1))"""
+l_r = list()
+l_n = list()
+for n in np.arange(50, 100, 25):
+    A = np.random.rand(n, n)
+    b = np.random.rand(n, 1)
+    x0 = np.zeros((n, 1))
 
+    x = GMRES(A, b, 1e-8)
+    mat_exp = A @ x
+    r = np.linalg.norm(mat_exp.reshape((n, 1)) - b)
+    l_r.append(r)
+    l_n.append(n)
+    """result = sc.gmres(A, b, x0)
+    mat_the = np.array(A @ result[0])
+    print(np.linalg.norm(mat_the.reshape((n, 1)) - b))"""
 
-A = np.random.rand(n, n)
-b = np.random.rand(n, 1)
-x0 = np.zeros((n, 1))
-print(A)
-print(b)
-
-x = GMRES(A, b, 1e-8)
-print(x)
-mat_exp = A @ x
-print(np.linalg.norm(mat_exp.reshape((n, 1)) - b))
-result = sc.gmres(A, b, x0)
-print(result[0])
-mat_the = np.array(A @ result[0])
-print(np.linalg.norm(mat_the.reshape((n, 1)) - b))
+plt.plot(l_n, l_r)
+plt.yscale('log')
+plt.show()
+plt.savefig('fig.png')
